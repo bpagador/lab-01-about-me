1c816223f273ed2ced5bf0f21f48f12da26a7767
Briseid Pagador <bpagador@gmail.com>
2020-03-30 18:32:47 -0700

    added some css changes

--- test/tests.js
+++ test/tests.js
@@ -6,12 +6,12 @@ test('yes, y, yasss is yes', function(assert) {
     const result = isYes('Y');
     const result1 = isYes('YES');
     const result2 = isYes('yes');
-    const result3 = isYes('uhhuh');
-    const result4 = isYes('yAsSs');
+    const result3 = isYes('yAsSs');
+    const result4 = isYes('yasss');
 
     assert.equal(result, true);
     assert.equal(result1, true);
     assert.equal(result2, true);
-    assert.equal(result3, false);
+    assert.equal(result3, true);
     assert.equal(result4, true);
 });
